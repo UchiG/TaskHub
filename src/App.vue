41b0172e782d2640ae31878795d636152dec56be
Uchirai <uchirai.govere@gmail.com>
2023-05-23 16:39:22 +0200

    onto other work

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <script setup>
-import Navbar from './components/Navbar.vue'
-import Footer from './components/Footer.vue'
+import Navbar from "./components/Navbar.vue"
+import Footer from "./components/Footer.vue"
 </script>
 
 <template>
- <Navbar />
- <router-view />
- <Footer />
+  <Navbar />
+  <router-view>
+    <transition>
+      <component :is="Component" />
+    </transition>
+  </router-view>
+  <Footer />
 </template>
 
 <style scoped>
+    .fade-enter-active, 
+    
+    .fade-leave-active {
+        transition: opacity 0.5s ease;
+    }
 
+    .fade-enter-from,
+
+    .fade-leave-to {
+        opacity: 0;
+    }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 
-export default createRouter({
+const router = createRouter({
     routes,
     history: createWebHistory(),
     linkActiveClass: 'active'
 })
+
+router.beforeEach((to, from) => {
+    // if (to.path === "/tasks") {
+    //     return { name: 'login' }
+    // }
+    if (to.meta.auth) {
+        return {
+            name: 'login',
+            query: {
+                redirect: to.fullPath
+            }
+    }
+
+    }
+})
+
+export default router;
