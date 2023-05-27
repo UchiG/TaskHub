0d60b6c0c291aa1f10a5ee126915b61e13972fbc
Uchirai <uchirai.govere@gmail.com>
2023-05-27 16:22:19 +0200

    more pages replaced

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,46 +1,46 @@
-import { defineStore } from 'pinia'
-import { computed, ref } from 'vue'
-import { csrfCookie, login, register, logout, getUser} from '../http/auth-api'
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
+import { csrfCookie, login, register, logout, getUser } from "../http/auth-api";
 
-export const useAuthStore = defineStore('auth', () => {
-    const user = ref(null)
+export const useAuthStore = defineStore("authStore", () => {
+  const user = ref(null);
 
-    const isLoggedIn = computed(() => !!user.value)
+  const isLoggedIn = computed(() => !!user.value);
 
-    const fetchUser = async () => {
-        try {
-            const { data } = await getUser()
-            user.value = data
-        } catch {
-            user.value = null
-        }
+  const fetchUser = async () => {
+    try {
+      const { data } = await getUser();
+      user.value = data;
+    } catch (error) {
+      user.value = null;
     }
+  };
 
-    const handleLogin = async (credentials) => {
-        await csrfCookie
-        await login(credentials)
-        await fetchUser()
-    }
+  const handleLogin = async (credentials) => {
+    await csrfCookie();
+    await login(credentials);
+    await fetchUser();
+  };
 
-    const handleRegister = async (newUser) => {
-        await register(newUser)
-        await handleLogin({
-            email: newUser.email,
-            password: newUser.password
-        })
-    }
+  const handleRegister = async (newUser) => {
+    await register(newUser);
+    await handleLogin({
+      email: newUser.email,
+      password: newUser.password,
+    });
+  };
 
-    const handleLogout = async () => {
-        await logout()
-        user.value = null
-    }
+  const handleLogout = async () => {
+    await logout();
+    user.value = null;
+  };
 
-    return {
-        user,
-        isLoggedIn,
-        fetchUser,
-        handleLogin,
-        handleRegister,
-        handleLogout
-    }
-})
+  return {
+    user,
+    isLoggedIn,
+    fetchUser,
+    handleLogin,
+    handleRegister,
+    handleLogout,
+  };
+});
